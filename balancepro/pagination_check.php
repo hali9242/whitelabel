09f9ab3ca637f5f9b57264ec4d244e9cbed21971
hali9242 <haiderali741993@gmail.com>
2024-12-04 18:21:13 +0500

    change on pagination for iotics main site and this file is in core folder

--- balancepro/pagination_check.php
+++ balancepro/pagination_check.php
@@ -406,7 +406,6 @@ $totalpages = ceil( $checkcountn / $limit );
 //echo "%%".$totalpages."%%";
 
 $output = '';
-<<<<<<< HEAD
 $output .= '<nav aria-label="balance pager m14-m15" balance-pager="" class="paging-holder clear" totalcheckcount="'.$checkcountn.'" totalpages="'.$totalpages.'">
     <ul class="pagination">';
 
@@ -475,116 +474,6 @@ if ($page < $totalpages) {
 
 $output .= '</ul>';
 $output .= '</nav>';
-=======
-$output .='<nav aria-label="balance pager m14-m15" balance-pager="" class="paging-holder clear">
-    <ul class="pagination">';
-
-    if(empty($page) or $page == '' or $page == 0 or $page == '0'){
-        $page = 1;
-    }
-								
-if ($page > 1) {
-	if($totalpages != 1){
-	$output .='<li>
-		<div class="prv-btn" sort="'.$sort.'" search="'.$search.'" lifestage="'.$lifestage.'" type="'.$dvaluen.'" pager="'.($page-1).'">
-			<div style="float:left;margin-right: 5px;margin-left: 10px;margin-top: 11px; cursor: pointer;">
-				<span class="btn-prev"></span>
-			</div>
-			<div style="float:left;margin-top: 7px;  cursor: pointer; margin-right: 22px;">
-				<span class="hidden-xs">Prev</span>
-			</div>
-		</div>
-	</li>';
-	}
-}
-								
-if($page == $totalpages){
-
-    if($totalpages == 1){
-                $output .='<li class="active" style="padding:5px 6px; cursor: pointer"  totalpages="'.$totalpages.'"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="1"  sort="'.$sort.'">1</li>';
-    } else if($totalpages<= 6) {
-        for ($i= 1 ; $i <= $totalpages; $i++) {
-            $output .='<li class="pg-btn '.($page == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer" sort="'.$sort.'" search="'.$search.'" lifestage="'.$lifestage.'" typevalue="'.$dvaluen.'" pagerv="'.$i.'">'.$i.'</li>';
-        }
-    } else if((6 + $page -1)<$totalpages){
-        for ($i= (1 + $page -1) ; $i <= (6 + $page -1) ; $i++) {
-            $output .='<li class="pg-btn '.($page == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer" sort="'.$sort.'" search="'.$search.'" lifestage="'.$lifestage.'" typevalue="'.$dvaluen.'" pagerv="'.$i.'">'.$i.'</li>';
-        }
-    } else{
-        for ($i= ($totalpages-5); $i <=  $totalpages ; $i++)
-        {
-            $output .='<li class="pg-btn '.($page == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer" sort="'.$sort.'" search="'.$search.'" lifestage="'.$lifestage.'" typevalue="'.$dvaluen.'" pagerv="'.$i.'">'.$i.'</li>';
-        }
-    }
-
-if ($page < $totalpages) {
-	$output .='<li><div class="next-btn" sort="'.$sort.'" search="'.$search.'" lifestage="'.$lifestage.'" type="'.$dvaluen.'" pager="'.($page+1).'" tab="sdff'.$page.'">
-		<div style="float:left;margin-right: 5px;margin-left: 22px;margin-top: 4px; cursor: pointer;"><span class="hidden-xs">Next</span></div>
-		<div style="float:left;margin-top: 10px;  cursor: pointer;"><span class="btn-next"></span></div>
-	</div></li>';
-}
-		if ($totalpages > 1) {
-		$output .='</ul>
-			<p>
-				<span>of&nbsp;</span>
-				<span class="ng-binding">'.$totalpages.'</span>
-				<span>&nbsp;pages</span>
-			</p>
-	</nav>';
-		} else {
-		$output .='</ul>
-			<p>
-				<span>of&nbsp;</span>
-				<span class="ng-binding">'.$totalpages.'</span>
-				<span>&nbsp;page</span>
-			</p>
-	</nav>';
-		}
-
-}else{
-
-    if($totalpages == 1){
-                $output .='<li class="active" style="padding:5px 6px; cursor: pointer"  totalpages="'.$totalpages.'"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="1"  sort="'.$sort.'">1</li>';
-    } else if($totalpages<= 6) {
-        for ($i= 1 ; $i <= $totalpages; $i++) {
-            $output .='<li class="pg-btn '.($page == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer" sort="'.$sort.'" search="'.$search.'" lifestage="'.$lifestage.'" typevalue="'.$dvaluen.'" pagerv="'.$i.'">'.$i.'</li>';
-        }
-    } else if((6 + $page -1)<$totalpages){
-        for ($i= (1 + $page -1) ; $i <= (6 + $page -1) ; $i++) {
-            $output .='<li class="pg-btn '.($page == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer" sort="'.$sort.'" search="'.$search.'" lifestage="'.$lifestage.'" typevalue="'.$dvaluen.'" pagerv="'.$i.'">'.$i.'</li>';
-        }
-    } else{
-        for ($i= ($totalpages-5); $i <=  $totalpages ; $i++)
-        {
-            $output .='<li class="pg-btn '.($page == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer" sort="'.$sort.'" search="'.$search.'" lifestage="'.$lifestage.'" typevalue="'.$dvaluen.'" pagerv="'.$i.'">'.$i.'</li>';
-        }
-    }
-
-if ($page < $totalpages) {
-	$output .='<li><div class="next-btn" sort="'.$sort.'" search="'.$search.'" lifestage="'.$lifestage.'" type="'.$dvaluen.'" pager="'.($page+1).'" tab="sdff'.$page.'">
-		<div style="float:left;margin-right: 5px;margin-left: 22px;margin-top: 4px; cursor: pointer;"><span class="hidden-xs">Next</span></div>
-		<div style="float:left;margin-top: 10px;  cursor: pointer;"><span class="btn-next"></span></div>
-	</div></li>';
-}
-		if ($totalpages > 1) {
-		$output .='</ul>
-			<p>
-				<span>of&nbsp;</span>
-				<span class="ng-binding">'.$totalpages.'</span>
-				<span>&nbsp;pages</span>
-			</p>
-	</nav>';
-		} else {
-		$output .='</ul>
-			<p>
-				<span>of&nbsp;</span>
-				<span class="ng-binding">'.$totalpages.'</span>
-				<span>&nbsp;page</span>
-			</p>
-	</nav>';
-		}
-}
->>>>>>> cfe35dfc852336d71269f5cd018e19ae0c9311c0
 								
 	$return_arr['message'] = $output;
 	echo json_encode($return_arr);
