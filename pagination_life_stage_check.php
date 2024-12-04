bc496fe8f86228e2c4cf9be5baea86c304fa819a
hali9242 <haiderali741993@gmail.com>
2024-12-04 14:02:19 +0500

    code for deployment on server

--- pagination_life_stage_check.php
+++ pagination_life_stage_check.php
@@ -428,7 +428,7 @@ if ($page < $totalpages) {
             <div style="float: left;   cursor: pointer;  align-items: center;">
         <span class="hidden-xs"></span>
     </div>
-    <div style="float: left;  cursor: pointer; margin-top:2px;  align-items: center;">
+    <div style="float: left;  cursor: pointer;   align-items: center;">
         <span class="btn-next"></span>
     </div>
         </div>
