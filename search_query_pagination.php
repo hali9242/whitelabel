522f420b75622fa2f8ef84939790e18bb715e504
hali9242 <haiderali741993@gmail.com>
2024-12-12 22:39:28 +0500

    latest pagination change

--- search_query_pagination.php
+++ search_query_pagination.php
@@ -340,65 +340,97 @@ $totalpages = ceil( $rcount / $limit );
 //echo $totalpages;
 $output = '';
     
-$output .= '<nav aria-label="balance pager m14-m15" balance-pager="" class="paging-holder clear">
-<ul class="pagination">';
-
-if ($pvalue > 1) {
-    if ($totalpages != 1) {
-        $output .= '<li>
-        <div class="search-prv-click" query="'.$searchvalue.'" pager="'.($pvalue-1).'" aria-label="Next" resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" sort="'.$sort.'">
-            <span class="btn-prev"></span>
-            <span class="hidden-xs">Prev</span>
-        </div>
-        </li>';
+    $output .='<nav aria-label="balance pager m14-m15" balance-pager="" class="paging-holder clear">
+    <ul class="pagination">';
+	if ($pvalue > 1) {
+		if($totalpages != 1){
+	$output .='<li>
+	<div class="search-prv-click" query="'.$searchvalue.'" pager="'.($pvalue-1).'" aria-label="Next"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'"   sort="'.$sort.'">
+		<span class="btn-prev"></span>
+	       <span class="hidden-xs">Prev</span>
+	</div>
+	</li>';
+		}
+	}
+	if(empty($pvalue) or $pvalue == '' or $pvalue == 0 or $pvalue == '0'){
+        $pvalue = 1;
     }
-}
+	if($pvalue == $totalpages){
+		//for ($i=1; $i <= min($totalpages,10); $i++) {
 
-if (empty($pvalue) || $pvalue == '' || $pvalue == 0 || $pvalue == '0') {
-    $pvalue = 1;
-}
+			if($totalpages == 1){
+				$output .='<li class="active" style="padding:5px 6px; cursor: pointer"  totalpages="'.$totalpages.'"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="1"  sort="'.$sort.'">1</li>';
+			} else if($totalpages<= 6) {
+				for ($i= 1 ; $i <= $totalpages; $i++) {
+					$output .='<li class="pg-btn-search '.($pvalue == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$i.'"  sort="'.$sort.'">'.$i.'</li>';
+				}
+			} else if((6 + $pvalue -1)<$totalpages){
+				for ($i= (1 + $pvalue -1) ; $i <= (6 + $pvalue -1) ; $i++) {
+					$output .='<li class="pg-btn-search '.($pvalue == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$i.'"  sort="'.$sort.'">'.$i.'</li>';
+				}
+			} else {
+				for ($i= ($totalpages-5); $i <=  $totalpages ; $i++)
+				{
+					$output .='<li class="pg-btn-search '.($pvalue == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$i.'"  sort="'.$sort.'">'.$i.'</li>';
+	             }
+			}
 
-if ($pvalue == $totalpages) {
-    if ($totalpages == 1) {
-        $output .= '<li class="active" style="padding:5px 6px; cursor: pointer" totalpages="'.$totalpages.'" resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="1" sort="'.$sort.'">1</li>';
-    } elseif ($totalpages <= 6) {
-        for ($i = 1; $i <= $totalpages; $i++) {
-            $output .= '<li class="pg-btn-search '.($pvalue == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer" resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$i.'" sort="'.$sort.'">'.$i.'</li>';
-        }
-    } else {
-        $output .= '<li class="pg-btn-search" style="padding:5px 6px; cursor: pointer" resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="1" sort="'.$sort.'">1</li>';
+		// 	for ($i= max(1, $pvalue); $i <= min($pvalue + 5, $totalpages); $i++) {	
+		// $output .='<li class="pg-btn-search '.($pvalue == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$i.'"  sort="'.$sort.'">'.$i.'</li>';
+		// }
 
-        if ($pvalue > 3) {
-            $output .= '<li class="pg-btn-search disabled" style="cursor: default;">...</li>';
-        }
 
-        $start = max(2, $pvalue - 1);
-        $end = min($totalpages - 1, $pvalue + 1);
+		if ($pvalue < $totalpages) {
+		$output .='<li style="padding-top: 4px; padding-left: 17px;">
+		<div class="search-nxt-click" style="cursor: pointer" query="'.$searchvalue.'" pager="'.($pvalue+1).'" resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" sort="'.$sort.'">
+			<span class="hidden-xs" style="float: left;">Next</span>
+			<span class="btn-next" style="float: left; margin-top: 6px; margin-left: 10px;"></span>
+		</div>
+		</li>';
+		}
+		
+		
+		$output .='</ul>
+			
+	</nav>';
+		}
+		else{
+	//for ($i=1; $i <= min($totalpages,10); $i++) {
 
-        for ($i = $start; $i <= $end; $i++) {
-            $output .= '<li class="pg-btn-search '.($pvalue == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer" resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$i.'" sort="'.$sort.'">'.$i.'</li>';
-        }
+		if($totalpages == 1){
+				$output .='<li class="active" style="padding:5px 6px; cursor: pointer"  totalpages="'.$totalpages.'"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="1"  sort="'.$sort.'">1</li>';
+		} else if($totalpages<= 6) {
+			for ($i= 1 ; $i <= $totalpages; $i++) {
+				$output .='<li class="pg-btn-search '.($pvalue == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$i.'"  sort="'.$sort.'">'.$i.'</li>';
+			}
+		} else if((6 + $pvalue -1)<$totalpages){
+			for ($i= (1 + $pvalue -1) ; $i <= (6 + $pvalue -1) ; $i++) {
+				$output .='<li class="pg-btn-search '.($pvalue == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$i.'"  sort="'.$sort.'">'.$i.'</li>';
+			}
+		} else {
+			for ($i= ($totalpages-5); $i <=  $totalpages ; $i++)
+			{
+				$output .='<li class="pg-btn-search '.($pvalue == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$i.'"  sort="'.$sort.'">'.$i.'</li>';
+			 }
+		}
 
-        if ($pvalue < $totalpages - 2) {
-            $output .= '<li class="pg-btn-search disabled" style="cursor: default;">...</li>';
-        }
-
-        $output .= '<li class="pg-btn-search" style="padding:5px 6px; cursor: pointer" resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$totalpages.'" sort="'.$sort.'">'.$totalpages.'</li>';
-    }
-}
-
-if ($pvalue < $totalpages) {
-    $output .= '<li style="padding-top: 4px; padding-left: 17px;">
-    <div class="search-nxt-click" style="cursor: pointer" query="'.$searchvalue.'" pager="'.($pvalue+1).'" resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" sort="'.$sort.'">
-        <span class="hidden-xs" style="float: left;">Next</span>
-        <span class="btn-next" style="float: left; margin-top: 6px; margin-left: 10px;"></span>
-    </div>
-    </li>';
-}
-
-$output .= '</ul>
-</nav>';
-
+	// 	for ($i= max(1, $pvalue ); $i <= min($pvalue + 5, $totalpages); $i++) {	
+	// $output .='<li class="pg-btn-search '.($pvalue == $i ? 'active' : '').'" style="padding:5px 6px; cursor: pointer"  resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" queryvalue="'.$searchvalue.'" pagerv="'.$i.'"  sort="'.$sort.'">'.$i.'</li>';
+	// }
+	if ($pvalue < $totalpages) {
+	$output .='<li style="padding-top: 4px; padding-left: 17px;">
+	<div class="search-nxt-click" style="cursor: pointer" query="'.$searchvalue.'" pager="'.($pvalue+1).'" resourcetypes="'.$resourcetypes.'" lifestage="'.$lifestage.'" sort="'.$sort.'">
+		<span class="hidden-xs" style="float: left;">Next</span>
+		<span class="btn-next" style="float: left; margin-top: 6px; margin-left: 10px;"></span>
+	</div>
+	</li>';
+	}
+		
+		$output .='</ul>
+			
+	</nav>';
+		}
+	
 //if($rcount==0)	$output='';
 $return_arr['message'] = $output;
 echo json_encode($return_arr);
